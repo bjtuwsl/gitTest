e707d135fe601f92274c965dee0f8759418aa81d
bjtuwsl <1783728365@qq.com>
2020-04-11 17:34:49 +0800

    4change

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 
 
-console.log(666)
+console.log(667)
 console.log(777)
 console.log(800)
 console.log(888)
@@ -8,8 +8,8 @@ console.log(104)
 console.log(105)
 console.log(208)
 console.log(335)
-console.log(1111)
+console.log(1117)
 alert(123)
 alert(456)
 alert(789)
-alert(999)
+alert(997)
